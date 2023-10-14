9ef2403db3c60cb76f0d09554a7103a8cc515fd3
Avenfenix <thedonjuan008p@gmail.com>
2023-10-14 10:44:13 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 sudo apt install -y build-essential libz-dev libpng-dev libsdl2-dev libfreetype-dev nasm libboost-dev libboost-filesystem-dev
 sudo apt-get install -y git
+sudo apt-get install -y freeglut3-dev and libglew-dev 
 git clone https://github.com/mupen64plus/mupen64plus-core
 
 cd mupen64plus-core
